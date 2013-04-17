d6264156cbaf8bf88012d791be8307a1cd249259
bjtal91@gmail.com <bjtal91@gmail.com@25cd940c-6eb6-f253-388b-d3c91abe4f25>
2013-04-17 09:34:44 +0000

    finished the data for the sliders

--- 5DAstro/src/Data/DataHolder.java
+++ 5DAstro/src/Data/DataHolder.java
@@ -1,5 +1,14 @@
 package Data;
 
-public class DataHolder {
+import GUI.SliderMenu;
 
+public class DataHolder {
+	
+	public int chosen3D = 0;
+	public int chosen2D = 1;
+	public int chosenGraph = 2;
+	
+	public DataHolder (){
+		new SliderMenu(this);
+	}
 }
