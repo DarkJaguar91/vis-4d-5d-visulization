367622023ed82d128a31253d6902995a338e9047
bjtal91@gmail.com <bjtal91@gmail.com@25cd940c-6eb6-f253-388b-d3c91abe4f25>
2013-04-17 17:23:30 +0000

    Commiting what you need ben DL now

--- 5DAstro/src/Data/DataHolder.java
+++ 5DAstro/src/Data/DataHolder.java
@@ -25,8 +25,61 @@ public class DataHolder {
 	}
 	
 	public int getFixedDimensionStep(int index){
-		
+		if (index == 0){
+			return sliderMenu.chooser3D.getValue();
+		}
+		else if (index == 1){
+			return sliderMenu.chooser2D.getValue();
+		}
+		else if (index == 2){
+			return sliderMenu.chooserGraph.getValue();
+		}
+		else {
+			return -1;
+		}
 	}
 	
-	public 
+	public float getMinFilter(int index){
+		if (index == 0){
+			float step = (data.getMaxData(0) - data.getMinData(0)) / (sliderMenu.range1.getMaximum() - sliderMenu.range1.getMinimum());
+			return sliderMenu.range1.getLowValue() * step;
+		}
+		else if (index == 1){
+			float step = (data.getMaxData(1) - data.getMinData(1)) / (sliderMenu.range2.getMaximum() - sliderMenu.range2.getMinimum());
+			return sliderMenu.range2.getLowValue() * step;
+		}
+		else if (index == 2){
+			float step = (data.getMaxData(2) - data.getMinData(2)) / (sliderMenu.range3.getMaximum() - sliderMenu.range3.getMinimum());
+			return sliderMenu.range3.getLowValue() * step;
+		}
+		else if (index == 3){
+			float step = (data.getMaxData(3) - data.getMinData(3)) / (sliderMenu.range4.getMaximum() - sliderMenu.range4.getMinimum());
+			return sliderMenu.range4.getLowValue() * step;
+		}
+		else {
+			return Float.MIN_VALUE;
+		}
+	}
+	
+	public float getMaxFilter(int index){
+		if (index == 0){
+			float step = (data.getMaxData(0) - data.getMinData(0)) / (sliderMenu.range1.getMaximum() - sliderMenu.range1.getMinimum());
+			return sliderMenu.range1.getHighValue() * step;
+		}
+		else if (index == 1){
+			float step = (data.getMaxData(1) - data.getMinData(1)) / (sliderMenu.range2.getMaximum() - sliderMenu.range2.getMinimum());
+			return sliderMenu.range2.getHighValue() * step;
+		}
+		else if (index == 2){
+			float step = (data.getMaxData(2) - data.getMinData(2)) / (sliderMenu.range3.getMaximum() - sliderMenu.range3.getMinimum());
+			return sliderMenu.range3.getHighValue() * step;
+		}
+		else if (index == 3){
+			float step = (data.getMaxData(3) - data.getMinData(3)) / (sliderMenu.range4.getMaximum() - sliderMenu.range4.getMinimum());
+			return sliderMenu.range4.getHighValue() * step;
+		}
+		else {
+			return Float.MIN_VALUE;
+		}
+	}
 }
