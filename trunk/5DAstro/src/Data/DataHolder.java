116db3d74ca6cbd5e77b8c3854fb0297011ddb11
bjtal91@gmail.com <bjtal91@gmail.com@25cd940c-6eb6-f253-388b-d3c91abe4f25>
2013-04-18 08:00:26 +0000

    Added graph features

--- trunk/5DAstro/src/Data/DataHolder.java
+++ trunk/5DAstro/src/Data/DataHolder.java
@@ -4,15 +4,28 @@ import GUI.GraphScreen;
 import GUI.SliderMenu;
 import GUI.frmPlot;
 
+/**
+ * 
+ * @author Brandon James Talbot
+ * 
+ *  Static class that contains all required variables for slider and plotting frames
+ */
 public class DataHolder {
 	
-	public static int[] fixedDimensions;
-	public static DataArray data;
+	//Global variables
+	public static int[] fixedDimensions; // the 3 fixed dimension states (first is 3d, 2d then graph)
+	public static DataArray data; // the data class being used
 	
-	public static SliderMenu sliderMenu;
-	public static frmPlot plotter;
-	public static GraphScreen plotterGraph;
+	public static SliderMenu sliderMenu; // the slider menu frame
+	public static frmPlot plotter; // the 3d jframe
+	public static GraphScreen plotterGraph; // the graph Jframe
 	
+	/**
+	 * Constructor
+	 * initialises
+	 * Reads in data
+	 * starts all frames
+	 */
 	public DataHolder (){
 		fixedDimensions = new int[3];
 		fixedDimensions[0] = 0;
@@ -28,15 +41,28 @@ public class DataHolder {
 		plotterGraph.setLocation(sliderMenu.getWidth(), plotter.getHeight());
 	}
 	
+	/**
+	 * Method that updates all plotters when something has changed
+	 */
 	public static void updatePlotter(){
 		plotter.reload();
 		plotterGraph.plotGraph();
 	}
 	
+	/**
+	 * Gets the fixed dimension for the specified plot (3d, 2d then graph) 
+	 * @param index The index (0 - 2)
+	 * @return The fixed dimension (0 - 3)
+	 */
 	public static int getFixedDimension(int index){
 		return fixedDimensions[index];
 	}
 	
+	/**
+	 * Gets the step for the fixed dimension (3d, 2d, graph)
+	 * @param index The index for the dimension (0 - 2)
+	 * @return The currently selected step value (as array index)
+	 */
 	public static int getFixedDimensionStep(int index){
 		if (index == 0){
 			return sliderMenu.step3D.getValue();
@@ -52,6 +78,13 @@ public class DataHolder {
 		}
 	}
 	
+	/**
+	 * Gets the minimum value for the Specified dimension
+	 * Note: for index 0 - 3 (returns array index)
+	 * Note: for index 4 (returns float value of heat)
+	 * @param index The index of the dimension
+	 * @return float value for minimum "value" of dimension
+	 */
 	public static float getMinFilter(int index){
 		if (index == 0){
 			return sliderMenu.range1.getLowValue();
@@ -74,6 +107,13 @@ public class DataHolder {
 		}
 	}
 	
+	/**
+	 * Gets the maximum value for the Specified dimension
+	 * Note: for index 0 - 3 (returns array index)
+	 * Note: for index 4 (returns float value of heat)
+	 * @param index The index of the dimension
+	 * @return float value for maximum "value" of dimension
+	 */
 	public static float getMaxFilter(int index){
 		if (index == 0){
 			return sliderMenu.range1.getHighValue();
