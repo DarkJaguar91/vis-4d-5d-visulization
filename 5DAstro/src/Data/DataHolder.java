e9b6f2436d466332b2133aceed8edf05071955aa
bjtal91@gmail.com <bjtal91@gmail.com@25cd940c-6eb6-f253-388b-d3c91abe4f25>
2013-04-19 13:43:38 +0000

    fixed slider issue were the Step selections labels did not change correctly

--- 5DAstro/src/Data/DataHolder.java
+++ 5DAstro/src/Data/DataHolder.java
@@ -34,7 +34,7 @@ public class DataHolder {
 		fixedDimensions[1] = 1;
 		fixedDimensions[2] = 2;
 		
-		data = new DataArray(10, 10, 10, 10, new String [] {"first", "second", "third", "forth"});
+		data = new DataArray(100, 100, 100, 100, new String [] {"first", "second", "third", "forth"});
 		SwingUtilities.invokeLater(
 				new Runnable(){
 					@Override
