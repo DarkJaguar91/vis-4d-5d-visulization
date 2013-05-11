0150c29423d9e91586285c2b35070d0083950648
bjtal91@gmail.com <bjtal91@gmail.com@25cd940c-6eb6-f253-388b-d3c91abe4f25>
2013-05-11 22:07:35 +0000

    created colour loading symbol to match the programs theme

--- 5DAstro/src/GUI/loadMenu.java
+++ 5DAstro/src/GUI/loadMenu.java
@@ -1,9 +1,14 @@
 package GUI;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class loadMenu extends JDialog{
 	/**
@@ -11,23 +16,35 @@ public class loadMenu extends JDialog{
 	 */
 	private static final long serialVersionUID = 4484316667280328406L;
 
+	Image img = null;
+	
 	public loadMenu(){
 		super();
 		setVisible(true);
-		JLabel l = new JLabel("Loading...");
-		l.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		
+		img = new ImageIcon("Image/loading.png").getImage();
+		JPanel l = new JPanel(){
+
+			/**
+			 * generated serial ID
+			 */
+			private static final long serialVersionUID = 585698405898269144L;
+			
+			@Override
+			public void paint (Graphics g){
+				if (img != null){
+					g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+				}
+			}
+		};
+		l.setPreferredSize(new Dimension(img.getWidth(this), img.getHeight(this)));
 		this.setLayout(new BorderLayout());
-		
 		this.add(l, BorderLayout.CENTER);
-		
 		this.pack();
 		
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		
-//		setUndecorated(true);
-//		getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+		
+		this.setLocationRelativeTo(null);
 	}
 }
